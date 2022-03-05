8bd6ccc211da127d1e2743a364124716e8ae4727
Guilherme <Gomes3654gui@gmail.com>
2022-03-05 15:12:17 -0300

    Primeira Atividade 03/03/22

--- Nona_Atividade/controller/Processo_Threads.java
+++ Nona_Atividade/controller/Processo_Threads.java
@@ -2,7 +2,6 @@ package controller;
 
 public class Processo_Threads extends Thread {
 	private int IdThread;
-	private int IdSequencial;
 	
 	public Processo_Threads(int IdThread) {
 		this.IdThread = IdThread;
@@ -10,18 +9,5 @@ public class Processo_Threads extends Thread {
 	@Override
 	public void run() {
 		System.out.println("Thread: " + IdThread);
-	}
-	public void Processo_Sequencial(int IdSequencial) {
-		this.IdSequencial = IdSequencial;
-	}
-	public void run_Sequencial() {
-		System.out.println("Thread: " + IdSequencial);
-		
-		int tempo = 1000;
-		try {
-			sleep(IdSequencial * tempo);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
 	}
 }
