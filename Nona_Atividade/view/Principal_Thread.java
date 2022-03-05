39214b0d57dcbd85445a710f24336265fdee5c0e
Guilherme <Gomes3654gui@gmail.com>
2022-03-05 14:53:03 -0300

    Atividade 03/03/22

---
+++ Nona_Atividade/view/Principal_Thread.java
@@ -0,0 +1,20 @@
+package view;
+/*
+1) Fazer uma aplicação que rode 5 Threads que
+cada uma delas imprima no console o seu
+número
+ */
+import controller.Processo_Threads;
+
+public class Principal_Thread {
+
+	public static void main(String[] args) {
+		System.out.println("Processo Thread Aleatorio:");
+		
+		for (int IdThread = 0; IdThread < 5; IdThread++) {
+			Processo_Threads process_thread = new Processo_Threads(IdThread);
+			process_thread.start();
+		}
+	}
+
+}
