058ff5cf5cf780ce31b948c52c5253e1ceb251fd
Guilherme <Gomes3654gui@gmail.com>
2022-03-05 18:58:12 -0300

    Decima Atividade

--- Decima_Atividade/view/Principal_Matriz.java
+++ Decima_Atividade/view/Principal_Matriz.java
@@ -1,5 +1,11 @@
 package view;
-
+/*
+2) Fazer uma aplicação que insira números
+aleatórios em uma matriz 3 x 5 e tenha 3
+chamadas de Threads, onde cada uma calcula a
+soma dos valores de cada linha, imprimindo a
+identificação da linha e o resultado da soma.
+ */
 import controller.Processo_Matriz;
 
 public class Principal_Matriz {
